da71d69438b5b5fe3f7f33c22fbc032b95ced4ab
Julian-Kota-Kikuchi <julian.kota@keio.jp>
2022-02-23 15:51:57 -0500

    feb 23

--- notebooks/modules/utils.py
+++ notebooks/modules/utils.py
@@ -9,8 +9,101 @@ import requests
 import json
 import datetime
 
+# libraries used to clean twitter data
+import re
+import nltk
+from nltk.corpus import stopwords
+nltk.download('stopwords')
+english_stop_words = stopwords.words('english')
 
-# methods for importing bitcoin price data
+
+### Methods to clean twitter data
+
+def repl(matchObj):
+    '''
+    
+    ''''
+    char = matchObj.group(1)
+    return "%s%s" % (char, char)
+
+
+def clean_tweet(tweet):
+    '''
+    Function to preprocess twitter sentences
+    Arguments:
+    tweet - tweet (string)
+    
+    Output - cleaned tweet (string)'''
+    
+    tweet = tweet.lower()
+    # remove URL "https://t.co/"
+    new_url = re.sub(r"https://t.co/[A-Za-z0-9]+", " ", tweet)
+    
+    # remove mention
+    tweet = re.sub(r"@[A-Za-z0-9!#%&*;_\$\.]+", " ", new_url)
+    
+    # remove # ,turning hashtags to the typical words.
+    tweet = re.sub(r"\W+", " ", tweet)
+    
+    # reduce character sequences >3 to 3
+    tweet = re.sub(re.compile(r"(\w)\1+"), repl, tweet)
+
+    # remove 2-character words
+    tweet = re.sub(r"\b[a-z]{1,2}\b", " ", tweet)
+    
+    # replace 2 or more spaces with a single space.
+    tweet = re.sub(r"\s+", " ", tweet)
+    
+    # remove spaces front and back
+    tweet = re.sub(r"^\s+|\s$", "", tweet)
+    
+    # remove stop words
+    tweet = ' '.join([word for word in tweet.split() 
+                      if word not in english_stop_words])
+    
+    return cleaned_tweet
+
+
+def remove_stop_words(tweet):
+    ''' 
+    '''
+    
+    tweet_removed = ' '.join([word for word in tweet.split() 
+                      if word not in english_stop_words])
+    
+    return tweet_removed
+
+
+def processed_twitter_data(link = '../data/interim/tweets_verified_2020-2021.pkl'):
+    '''Function to return preprcessed twitter data
+    Arguments:
+    link - link to the pkl data
+    
+    Output:
+    df - dataframe of cleaned twitter'''
+    
+    # first download twitter data
+    df = pd.read_pickle(link)
+    
+    # apply first cleaning on the tweets
+    # language EN only
+    df = df[df['language'] == 'en']
+
+    # apply cleaninging 1
+    df['tweet'] = df['tweet'].progress_apply(clean_tweet_1)
+    
+    
+    
+    return df
+
+
+
+
+
+
+
+
+### methods for importing bitcoin price data
 def get_data(symbol_name, exchange = 'BINANCE',start_date = "2020-01-01"):
     
     # instanciate the library and get historiacal data
@@ -45,11 +138,6 @@ def getapi(api, start_date = "2020-01-01"):
     
     return df
 
-
-
-
-
-
 def calculate_return(df, columns, n):
     '''
     function to calculate returns
@@ -68,7 +156,6 @@ def calculate_return(df, columns, n):
         df[columns+'_return'] = df[columns].pct_change(n)
 
     return df
-
 
 
 
