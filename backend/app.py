d835759291696c8f60ed35e5ab3ba6db064e45ff
Kevin Chakornsiri <kevin.chakorn@gmail.com>
2022-04-21 21:37:17 -0400

    Add "get prediction"

--- backend/app.py
+++ backend/app.py
@@ -12,79 +12,38 @@ CORS(app)
 
 
 
-# @app.route("/update_sentiment")
-# def update_sentiment():
-#   df = scrape.scrapeToday()
-#   df = clean.cleanTweets(df)
-#   df = extract.extractSentiment(df)
-#   df = extract.extractWeightedSentiment(df)
-
-#   sentiment = df.iloc[0]["weighted_sentiment"]
-
-#   sentiment_json = {"sentiment": sentiment}
-
-#   with open('sentiment.json', 'w') as json_file:
-#     json.dump(sentiment_json, json_file)
-
-#   return jsonify(sentiment_json)
-
-
-
-# @app.route("/get_sentiment")
-# def get_sentiment():
-
-#   with open('sentiment.json') as json_file:
-#     sentiment_json = json.load(json_file)
-
-#   return jsonify(sentiment_json), 200
-
-
-
-# @app.route("/predict_with_sentiment")
-# def predict_with_sentiment():
-
-#   with open('sentiment.json') as json_file:
-#     sentiment_json = json.load(json_file)
-  
-#   predicted_value = prediction.predict_to_string(sentiment_json["sentiment"])
-
-#   return jsonify({"predicted_value_with_sentiment": predicted_value})
-
-
-
-# @app.route("/predict_without_sentiment")
-# def predict_without_sentiment():
-#   predicted_value = prediction_no_sentiment.predict_to_string()
-
-#   return jsonify({"predicted_value_without_sentiment": predicted_value})
-
-
-
 @app.route("/get_predictions")
+def get_predictions():
+  df = pd.read_pickle("./data/predictions.pkl")
+  res = df.head(7).to_json(orient="records")
+
+  return Response(res, mimetype='application/json'), 200
+
+
+
+@app.route("/update_predictions")
 def update_predictions():
   df = pd.read_pickle("./data/predictions.pkl")
-  pred_without_sentiment = prediction_no_sentiment.predict_to_string()
-  res = df.head(10).to_json(orient="records")
+  pred_without_sentiment, open_bitcoin = prediction_no_sentiment.predict_to_string()
+  df.loc[0, "actual_value"] = open_bitcoin
 
-  if (pred_without_sentiment == df.loc[0, "pred"]):
-    res = df.head(10).to_json(orient="records")
-  else:
-    sentiment = get_sentiment()
-    pred_with_sentiment = prediction.predict_to_string(sentiment)
+  sentiment = get_sentiment()
+  pred_with_sentiment = prediction.predict_to_string(sentiment)
 
-    new_row = pd.DataFrame({
-      "index": [df.shape[0]],
-      "date": [datetime.now()],
-      "sentiment": [sentiment], 
-      "pred": [pred_without_sentiment], 
-      "pred_sent": [pred_with_sentiment]
-    })
+  new_row = pd.DataFrame({
+    "index": [df.shape[0]],
+    "date": [datetime.now()],
+    "sentiment": [sentiment], 
+    "pred": [pred_without_sentiment], 
+    "pred_sent": [pred_with_sentiment],
+    "actual_value": 0,
+  })
 
-    df = pd.concat([new_row, df], ignore_index=True)
-    df.to_pickle("./data/predictions.pkl")
-    res = df.head(10).to_json(orient="records")
+  df = pd.concat([new_row, df], ignore_index=True)
+  df.to_pickle("./data/predictions.pkl")
+  res = df.head(7).to_json(orient="records")
   
-  return Response(res, mimetype='application/json')
+  return Response(res, mimetype='application/json'), 200
 
 def get_sentiment():
   df = scrape.scrapeToday()
